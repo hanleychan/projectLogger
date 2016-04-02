a5952126a1310131ee69f1b418218aca6e20d775
Hanley Chan <hanleychan@gmail.com>
2016-04-02 12:30:53 -0700

    Added initial twig template for root route

--- src/public/index.php
+++ src/public/index.php
@@ -5,8 +5,25 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require('../vendor/autoload.php');
 
 $app = new \Slim\App;
+
+// Get container
+$container = $app->getContainer();
+
+// Register view component on container
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('../templates', [
+        'cache' => false
+    ]);
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $c['router'],
+        $c['request']->getUri()
+    ));
+
+    return $view;
+};
+
 $app->get('/', function (Request $request, Response $response) {
-    return "HOME PAGE";
+    return $this->view->render($response, 'index.twig');
 })->setName("home");
 
 $app->run();
