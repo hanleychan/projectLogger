c119aaec9e249844a6ba4a4991062680accf95ca
Hanley Chan <hanleychan@gmail.com>
2016-04-02 12:45:15 -0700

    Added slim flash messages to project

--- src/public/index.php
+++ src/public/index.php
@@ -1,10 +1,13 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use \Slim\App;
 
 require('../vendor/autoload.php');
 
-$app = new \Slim\App;
+$config["displayErrorDetails"] = true;
+
+$app = new App(["settings" => $config]);
 
 // Get container
 $container = $app->getContainer();
@@ -21,6 +24,18 @@ $container['view'] = function ($c) {
 
     return $view;
 };
+
+// Register flash component on container
+$container['flash'] = function ($c) {
+    session_start();
+    return new \Slim\Flash\Messages();
+};
+
+// Add middleware
+$app->add(function (Request $request, Response $response, callable $next) {
+    $this->view->offsetSet('flash', $this->flash);
+    return $next($request, $response);
+});
 
 $app->get('/', function (Request $request, Response $response) {
     return $this->view->render($response, 'index.twig');
