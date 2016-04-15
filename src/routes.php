c358780406ab7655579fbd28c49180673b9fd3fe
Hanley Chan <hanleychan@gmail.com>
2016-04-15 12:54:02 -0700

    Implemented transfer project ownership

--- src/routes.php
+++ src/routes.php
@@ -1046,9 +1046,56 @@ $app->post('/project/{name}/transferOwnership/confirm', function ($request, $res
         return $response->withRedirect($router->pathFor('fetchProjectLogs', compact("name")));
     }
 
-    return $this->view->render($response, 'project.twig', compact("user", "page", "owner"));
+    return $this->view->render($response, 'project.twig', compact("user", "page", "project", "owner"));
 })->setName('confirmTransferOwnership');
 
+$app->post('/project/{name}/transferOwnership/{newOwner}', function ($request, $response, $args) {
+    $router = $this->router;
+    // Redirect to login page if not logged in
+    if(!$this->session->isLoggedIn()) {
+        return $response->withRedirect($router->pathFor('login'));
+    }
+
+    $name = trim($args["name"]);
+    $newOwner = trim($args["newOwner"]);
+    $action = trim($request->getParam("action"));
+    $user = User::findById($this->db, $this->session->userID);
+
+    // Fetch project
+    if(!$project = Project::findProjectByName($this->db, $name)) {
+        $this->flash->addMessage("fail", "Project does not exist");
+        return $response->withRedirect($route->pathFor('projects'));
+    }
+
+    // Check if user is the owner
+    if($project->ownerID !== $user->id) {
+        $this->flash->addMessage("fail", "You do not have permission to view this page");
+        return $response->withRedirect($router->pathFor('fetchProjectLogs', compact("name")));
+    }
+
+    // Fetch new owner
+    if(!$projectMember = ProjectMember::findProjectMemberByProjectNameAndUsername($this->db, $name, $newOwner)) {
+        $this->flash->addMessage("fail", "There was an error processing your request");
+        return $response->withRedirect($router->pathFor('fetchProjectLogs', compact("name")));
+    }
+
+    if($action === 'yes') {
+        $project->ownerID = $projectMember->userID;
+        $project->save();
+
+        // Make new owner admin
+        if($owner->isAdmin = false) {
+            $owner->isAdmin = true;
+            $owner->save();
+        }
+
+        $this->flash->addMessage("success", "Project ownership has been transfered to {$projectMember->username}");
+        return $response->withRedirect($router->pathFor('projectActions', compact("name")));
+    } elseif ($action !== 'no') {
+        $this->flash->addMessage("fail", "There was an error processing your request");
+    }
+        return $response->withRedirect($router->pathFor('transferOwnership', compact("name")));
+})->setName('processTransferOwnership');
 
 
 /**
